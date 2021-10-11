8693b4e358fd6d0c5c72f38db132cc30e6d7f8f1
clivercolongne <clivercolongne@gmail.com>
2021-10-11 10:05:26 -0300

    entrega final

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,40 +5,39 @@ import { getFirestore } from "../firebase/Firebase";
 import { CartContext } from "../context/CartContext";
 const Form = (props) => {
     const { cart, clear, precioTotal } = useContext(CartContext);
-    const initialForm ={
-        nombre:"",
-        email:"",
-        tel:"",
+    const initialForm = {
+        nombre: "",
+        email: "",
+        tel: "",
     };
-  const [formData, setFormData] = useState(initialForm)
-  function handleChange(e){
-    setFormData(
-        {
-        ...formData,
-        [e.target.name]: e.target.value
+    const [formData, setFormData] = useState(initialForm)
+    function handleChange(e) {
+        setFormData(
+            {
+                ...formData,
+                [e.target.name]: e.target.value
+            }
+        )
     }
-    )
-  }
-  function handleSubmit(e)
-    {
-    e.preventDefault()
-    const newOrder={
-        buyer: formData,
-        items: cart,
-        date: firebase.firestore.Timestamp.fromDate(new Date()),
-        total: precioTotal()
-                    }
-    console.log(newOrder)
-    const db = getFirestore()
-    const orders = db.collection('orders')
-   orders.add(newOrder)
-  .then(resp => alert(`la orden de compra es: ${resp.id}`))
-  .catch(err => console.log(err))
-  .finally(()=>{
-       setFormData(initialForm)
-     clear()
-  })
-  }
+    function handleSubmit(e) {
+        e.preventDefault()
+        const newOrder = {
+            buyer: formData,
+            items: cart,
+            date: firebase.firestore.Timestamp.fromDate(new Date()),
+            total: precioTotal()
+        }
+        console.log(newOrder)
+        const db = getFirestore()
+        const orders = db.collection('orders')
+        orders.add(newOrder)
+            .then(resp => alert(`la orden de compra es: ${resp.id}`))
+            .catch(err => console.log(err))
+            .finally(() => {
+                setFormData(initialForm)
+                clear()
+            })
+    }
 
     return (
         <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
@@ -47,23 +46,31 @@ const Form = (props) => {
             </Modal.Header>
             <Modal.Body>
                 <fieldset>
-                    <form onSubmit={handleSubmit}  onChange={handleChange} >
-                        <label>Nombre:</label>
-                        <input type="text" placeholder="ingresa nombre" name="nombre" value={formData.nombre}/>
-                      
-                        <label>Email:</label>
-                        <input type="email" placeholder="ingresa email" name="email" value={formData.email}/>
-                        <label>Telefono:</label>
-                        <input type="text"  placeholder="ingresa tel" name="tel" value={formData.tel}/>
-                        <button variant="outline-info" type="submit" className="mt-auto" >
-                            Compra"
+                    <form onSubmit={handleSubmit} onChange={handleChange} >
+                        <div>
+
+
+                            <label>  Nombre:   </label>
+                            <input type="text" placeholder="ingresa nombre" name="nombre" value={formData.nombre} />
+                        </div>
+                        <div>
+                            <label>  Email:   </label>
+                            <input type="email" placeholder="ingresa email" name="email" value={formData.email} />
+                        </div>
+                        <div>
+                            <label>  Telefono:   </label>
+                            <input type="text" placeholder="ingresa tel" name="tel" value={formData.tel} />
+                        </div>
+                        <button variant="outline-info" type="submit" className="btn btn-success" >
+
+                            Compra
                         </button>
                     </form>
                 </fieldset>
             </Modal.Body>
             <Modal.Footer>
-                <button variant="outline-info" type="submit" className="mt-auto" onClick={clear}>
-                    cerra"
+                <button variant="outline-info" type="submit" className="btn btn-danger" onClick={clear}>
+                    Cerrar
                 </button>
             </Modal.Footer>
 
